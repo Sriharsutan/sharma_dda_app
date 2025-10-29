bb4b986c2806f997db359c2f49caae2d47906d16
sriharsutan <sriharsutan@gmail.com>
2025-10-29 20:32:19 +0530

    project level gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@
 //    alias(libs.plugins.kotlin.compose) apply false
 //}
 //
-
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 tasks.register("clean", Delete::class) {
